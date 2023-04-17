c786044a9dc1709e2548888298e8b95af76abf6e
shiwei93 <stayfocusjs@gmail.com>
2023-04-17 22:59:39 +0800

    feat: upgrade react native version

--- template/prettier.config.js
+++ template/prettier.config.js
@@ -2,15 +2,8 @@ module.exports = {
     arrowParens: 'avoid',
     singleQuote: true,
     printWidth: 100,
-    tabWidth: 4,
+    tabWidth: 2,
     trailingComma: 'all',
     semi: true,
-    overrides: [
-        {
-            files: ['*.json', '*.yaml', '*.yml', '*.jsonc'],
-            options: {
-                tabWidth: 2,
-            },
-        },
-    ],
-};
+  };
+  
